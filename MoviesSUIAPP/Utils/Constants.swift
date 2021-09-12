ca44e7d825f9e8c988c3ee35534574db54dfa5b7
Jorge Encinas <jorge.encinas@devant.com.mx>
2021-09-12 17:15:01 -0500

    Initial

---
+++ MoviesSUIAPP/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MoviesSUIAPP
+//
+//  Created by Jorge Encinas on 12/09/21.
+//
+
+import Foundation
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "ce7b7076" // OMDBAPI
+}
